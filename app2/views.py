955df9c08350807eea3b027df6b35919814e07cf
yfy <yefengyun241@163.com>
2018-12-26 15:05:03 +0800

    装饰器的添加和动态路由的更改

--- app2/views.py
+++ app2/views.py
@@ -5,9 +5,39 @@ from app2 import common
 from app2 import htmlgen
 
 
+def Filter(before_func, after_func):
+    def outer(main_func):
+        def wrapper(request, *args, **kwargs):
+
+            before_result = before_func(request, *args, **kwargs)
+            if (before_result != None):
+                return before_result
+
+            main_result = main_func(request, *args, **kwargs)
+            if main_result != None:
+                return main_result
+
+            after_result = after_func(request, *args, **kwargs)
+            if after_result != None:
+                return after_result
+
+        return wrapper
+
+    return outer
+
+
+def after_index(request, *args, **kwargs):
+    print("after")
+
+
+def before_index(request, *args, **kwargs):
+    print("before")
+
+
 # Create your views here.
-def index(request, page):
-    page = common.string_int(page, 1)
+@Filter(before_index, after_index)
+def index(request, *args, **kwargs):
+    page = common.string_int(kwargs['page'], 1)
 
     cookies = request.COOKIES
     per_item = common.string_int(cookies['page_num'], 5)
--- djangofrom/DynamicRouter.py
+++ djangofrom/DynamicRouter.py
@@ -3,30 +3,26 @@ from django.shortcuts import HttpResponse, redirect
 
 
 def matchURL(request, **kwargs):
-    app = kwargs['app']
-    func = kwargs['function']
-    print(app,func)
-    try:
-        page = kwargs['page']
-    except Exception as e:
-        print("not page")
+    appandfun = ['app', 'function']
+    app = kwargs[appandfun[0]]
+    func = kwargs[appandfun[1]]
+
+    newkwargs = {}
+
+    for key, value in kwargs.items():
+        if key not in appandfun:
+            newkwargs[key] = value
 
     try:
         appObj = __import__('{}.views'.format(app))
         viewObj = getattr(appObj, 'views')
         funcObj = getattr(viewObj, func)
-        try:
-            if page:
-                result = funcObj(request, page)
-        except Exception as e:
-            print(e)
-            result = funcObj(request)
-
+        result = funcObj(request, **newkwargs)
     except (ImportError, AttributeError) as e:
         print(e)
         # 导入失败时，自定义404错误
         return HttpResponse('404 Not Found')
     except Exception as e:
         print(e)
-        return redirect('/{}/index/1/'.format(app))
+        return HttpResponse('404 Error')
     return result
